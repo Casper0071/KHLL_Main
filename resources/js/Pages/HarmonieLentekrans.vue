ada0fc869f07d3a5d530457046ee4e92d8fb0950
casper <casper.vocking@gmail.com>
2026-08-17 15:48:59 +0200

    blob responcivenes fix harmonie en opleidingsOrkest

--- resources/js/Pages/HarmonieLentekrans.vue
+++ resources/js/Pages/HarmonieLentekrans.vue
@@ -25,25 +25,12 @@
             minHeight="400px"
         />
 
-        <!-- Mobiele blob achtergrond -->
-        <div class="blob1Mobile"></div>
 
         <!-- Eerste sectie met blob -->
         <div class="blob">
             <div class="blob1">
-                <svg v-if="windowWidth >= 2000" width="1324" height="447" viewBox="0 0 1324 447" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M861 22.4834C618.464 -3.2693 137.5 -33.6513 0 105.849V381.483C171.5 467.983 642.396 487.061 768 309.483C840.5 206.983 1327.5 304.483 1322.5 163.483C1317.5 22.4834 1176.5 55.9834 861 22.4834Z" fill="#F4F7FB" stroke="black"/>
-                </svg>
-                <svg v-else-if="windowWidth >= 1600" width="1331" height="508" viewBox="0 0 1331 508" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M861 22.4834C618.464 -3.2693 137.5 -33.6513 0 105.849V442.983C171.5 529.483 642.396 548.561 768 370.983C840.5 268.483 1335 316.983 1330 175.983C1325 34.9834 1176.5 55.9834 861 22.4834Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 1000" width="1302" height="1209" viewBox="0 0 1302 1209" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M869.5 18.3689C626.427 -1.69807 138 -33.131 0.5 106.369V1104.87C85.5 1245.87 800.5 1266.37 841 1016.37C911.51 581.121 1283 535.869 1300.5 272.869C1309.87 132.092 1190.5 44.8691 869.5 18.3689Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 1150" width="1322" height="972" viewBox="0 0 1322 972" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M867.5 28.7862C625.916 -4.74479 138 -35.2137 0.5 104.286V865.286C85.5 1006.29 744.85 1029.03 811 794.286C867.5 593.786 1321 486.286 1321 228.286C1321 87.1976 1166.5 70.2864 867.5 28.7862Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else width="1322" height="783" viewBox="0 0 1322 783" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                <svg width="1322" height="783" viewBox="0 0 1322 783" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M867.5 28.7862C625.916 -4.74479 138 -35.2137 0.5 104.286V651.786C85.5 792.786 620 849.286 779.5 664.786C1026.15 379.477 1326 369.286 1321 228.286C1316 87.2862 1166.5 70.2864 867.5 28.7862Z" fill="var(--surface)"/>
                 </svg>
             </div>
@@ -187,22 +174,18 @@ onUnmounted(() => {
 }
 
 .blob1 {
-    @apply absolute;
-    margin-top: 100px;
-    width: 100vw;
-    top: 55%;
+    @apply absolute overflow-hidden left-0;
+    width: 95%;
+    min-height: 400px;
+    height: 750px;
+    top: 65%;
     transform: translateY(-50%);
 }
 
 .blob1 svg {
-    width: 90vw;
+    @apply absolute w-full h-full;
 }
 
-.blob1Mobile {
-    @apply absolute hidden w-screen h-full;
-    background: var(--surface);
-    clip-path: shape(from 0 25px, curve to 30% 19px with 20% 12px, curve to 50% 18px with 40% 26px, curve to 70% 12px with 60% 10px, curve to 100% 25px with 80% 14px, vline to calc(100% - 25px), curve to 70% calc(100% - 22px) with 80% calc(100% - 3px), curve to 50% calc(100% - 39.5px) with 60% calc(100% - 41px), curve to 30% calc(100% - 27.5px) with 40% calc(100% - 38px), curve to 0 calc(100% - 25px) with 20% calc(100% - 17px), vline to 0);
-}
 
 /* ============================================
    MUZIEKNOTEN ACHTERGROND
@@ -227,124 +210,58 @@ onUnmounted(() => {
 
 /* Desktop Large */
 @media (min-width: 2300px) {
-    .blob1 svg {
-        scale: 1.8;
-    }
-    .blob1 {
-        transform: translateY(-65%);
-    }
+
     .musicnote1{
         scale: 1.8;
     }
 }
 
 @media (min-width: 2250px) and (max-width: 2299px) {
-    .blob1 svg {
-        scale: 1.7;
-    }
+
     .musicnote1{
         scale: 1.6;
     }
 }
 
-@media (min-width: 2000px) and (max-width: 2249px) {
-    .blob1 svg {
-        scale: 1.6;
-    }
-}
-
-@media (min-width: 1900px) and (max-width: 1999px) {
-    .blob1 svg {
-        scale: 1.5;
-    }
-}
-
-@media (min-width: 1600px) and (max-width: 1899px) {
-    .blob1 svg {
-        width: 100vw;
-        scale: 1.3;
-        margin-left: -100px;
-    }
-    .blob1 {
-        transform: translateY(-60%);
-    }
-}
-
-@media (min-width: 1450px) and (max-width: 1599px) {
-    .blob1 svg {
-        width: 90vw;
-        scale: 1.1;
-    }
-}
-
-@media (min-width: 1350px) and (max-width: 1449px) {
-    .blob1 svg {
-        width: 90vw;
-    }
-}
-
-@media (max-width: 1350px) {
-    .blob1 svg {
-        width: 100vw;
-    }
-}
-
-@media (max-width: 1200px) {
-    .blob1 {
-        margin-top: 50px;
+@media (min-width: 2100px) {
+    .blob1{
+        top: 60%;
+        height: 800px;
     }
 }
 
 @media (max-width: 1150px) {
-    .blob1 {
-        margin-top: 100px;
-    }
     .content2 {
         width: 90%;
         margin-top: 250px;
     }
 }
 
-@media (max-width: 1100px) {
-    .blob1 {
-        margin-top: 50px;
-    }
-    .content2 {
-        width: 90%;
-        margin-top: 250px;
-    }
-}
+@media (max-width: 1023px) {
 
-@media (max-width: 1000px) {
-    .blob1 {
-        margin-top: 100px;
-    }
-}
-
-@media (max-width: 950px) {
-    .blob1 {
-        margin-top: 50px;
-    }
-}
-
-@media (max-width: 850px) {
-    .blob1 {
-        visibility: hidden;
-    }
-    .blob1Mobile {
-        @apply block;
-        height: 1000px;
-    }
-    .content2 {
-        width: 100%;
-        margin-top: 150px;
-    }
-    .blob {
-        filter: none;
+    .blob1{
+        height: 1200px;
+        width: 130%;
     }
     .musicnote1 {
         margin-top: 1300px;
     }
+}
+
+@media (max-width: 1100px) {
+
+    .content2 {
+        width: 90%;
+        margin-top: 250px;
+    }
+}
+
+@media (max-width: 850px) {
+    .content2 {
+        width: 100%;
+        margin-top: 300px;
+    }
+
 }
 
 @media (max-width: 600px) {
@@ -359,22 +276,7 @@ onUnmounted(() => {
     }
 }
 
-@media (max-width: 550px) {
-    .blob1Mobile {
-        height: 1200px;
-    }
-}
-
-@media (max-width: 430px) {
-    .blob1Mobile {
-        height: 1300px;
-    }
-}
-
 @media (max-width: 354px) {
-    .blob1Mobile {
-        height: 1300px;
-    }
     .item1 {
         width: 145px;
     }
--- resources/js/Pages/OpleidingsOrkest.vue
+++ resources/js/Pages/OpleidingsOrkest.vue
@@ -49,22 +49,8 @@
         <!-- Eerste sectie met blob -->
         <div class="blob">
             <div class="blob1">
-                <svg v-if="windowWidth <= 400" width="1442" height="2634" viewBox="0 0 1442 2634" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M493.5 22.528C266.5 53.5268 135.667 -14.6389 0 53.5277V2553.03C0 2581.69 40.9 2630.13 204.5 2594.53C409 2550.03 597 2546.53 896.5 2594.53C1136.1 2632.93 1359.67 2657.53 1441.5 2594.53V53.5281C1441.5 53.5281 1284 93.5253 1106 53.5281C883.128 3.44792 811.772 -20.9347 493.5 22.528Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 500" width="1442" height="1999" viewBox="0 0 1442 1999" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M493.5 22.528C266.5 53.5268 135.667 -14.6389 0 53.5277V1918.03C0 1946.69 40.9 1995.13 204.5 1959.53C409 1915.03 597 1911.53 896.5 1959.53C1136.1 1997.93 1359.67 2022.53 1441.5 1959.53V53.5281C1441.5 53.5281 1284 93.5253 1106 53.5281C883.128 3.44792 811.772 -20.9347 493.5 22.528Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 700" width="1442" height="1500" viewBox="0 0 1442 1500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M493.5 22.528C266.5 53.5268 135.667 -14.6389 0 53.5277V1419.03C0 1447.69 40.9 1496.13 204.5 1460.53C409 1416.03 597 1412.53 896.5 1460.53C1136.1 1498.93 1359.67 1523.53 1441.5 1460.53V53.5281C1441.5 53.5281 1284 93.5253 1106 53.5281C883.128 3.44792 811.772 -20.9347 493.5 22.528Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 850" width="1442" height="1285" viewBox="0 0 1442 1285" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M487 37.8838C270.61 98.0109 135.667 22.217 0 90.3836V1204.38C0 1233.05 40.9 1281.48 204.5 1245.88C409 1201.38 597 1197.88 896.5 1245.88C1136.1 1284.28 1359.67 1308.88 1441.5 1245.88V82.3837C1436.5 82.8837 1274 122.381 1096 82.3837C873.128 32.3036 796.5 -48.1153 487 37.8838Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 1000" width="1274" height="785" viewBox="0 0 1274 785" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M530 22.0063C323.2 52.0063 92.5 56.0063 -0.5 84.5063V757.506C207.135 851.473 444.5 664.039 776 686.006C1041.5 703.6 1309.5 419.006 1270 185.006C1233.85 -29.1472 788.5 -15.4937 530 22.0063Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else width="1274" height="594" viewBox="0 0 1274 594" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                <svg width="1274" height="594" viewBox="0 0 1274 594" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M530 22.0063C323.2 52.0063 92.5 56.0063 -0.5 84.5063V560.507C207.135 654.473 453.5 514.04 785 536.007C1050.5 553.6 1309.5 419.006 1270 185.006C1233.85 -29.1472 788.5 -15.4937 530 22.0063Z" fill="var(--surface)"/>
                 </svg>
             </div>
@@ -105,29 +91,7 @@
         <!-- Derde sectie met blob -->
         <div class="blob">
             <div class="blob2">
-                <svg v-if="windowWidth <= 360" width="1437" height="2884" viewBox="0 0 1437 2884" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M701 1.39167C875 -10.2083 1264.17 53.5583 1437 86.8917V2883.78C1237.5 2814.28 1176 2669.79 966 2513.79C513.099 2177.34 365.5 2381.79 0 2110.79V48.3916C161.167 37.5583 527 12.9917 701 1.39167Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 425" width="1437" height="2584" viewBox="0 0 1437 2584" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M701 1.3916C875 -10.2084 1264.17 53.5583 1437 86.8916V2583.78C1237.5 2514.28 1176 2369.79 966 2213.79C513.099 1877.35 365.5 2081.79 0 1810.79V48.3915C161.167 37.5582 527 12.9916 701 1.3916Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 475" width="1437" height="2247" viewBox="0 0 1437 2247" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M701 1.39167C875 -10.2083 1264.17 53.5583 1437 86.8917V2246.78C1237.5 2177.28 1176 2032.79 966 1876.79C513.099 1540.35 365.5 1744.78 0 1473.78V48.3916C161.167 37.5583 527 12.9917 701 1.39167Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 530" width="1437" height="1840" viewBox="0 0 1437 1840" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M701 1.39167C875 -10.2083 1264.17 53.5583 1437 86.8917V1839.89C1237.5 1770.39 1176 1625.89 966 1469.89C513.099 1133.45 365.5 1337.89 0 1066.89V48.3916C161.167 37.5583 527 12.9917 701 1.39167Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 600" width="1437" height="1667" viewBox="0 0 1437 1667" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M701 1.39167C875 -10.2083 1264.17 53.5583 1437 86.8917V1666.89C1237.5 1597.39 1176 1452.89 966 1296.89C513.099 960.451 365.5 1164.89 0 893.888V48.3916C161.167 37.5583 527 12.9917 701 1.39167Z" fill="var(--surface)"/>
-                </svg>
-                <svg v-else-if="windowWidth <= 850" width="1437" height="1433" viewBox="0 0 1437 1433" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M701 1.39167C875 -10.2083 1264.17 53.5583 1437 86.8917V1432.39C1237.5 1362.89 1176 1218.39 966 1062.39C513.099 725.951 365.5 930.389 0 659.389V48.3916C161.167 37.5583 527 12.9917 701 1.39167Z" fill="var(--surface)"/>
-                </svg>
-                <svg  v-else-if="windowWidth <= 1500" width="1379" height="1141" viewBox="0 0 1379 1141" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1043.09 86.4999C1248.79 104.824 1331.09 185 1379.09 213V1140C1258.59 1130.5 532.592 665 276.592 647.5C20.5923 630 -74.2825 271.878 64.0924 143C217.092 0.500528 349.592 0.50027 488.091 0.5H488.093C596.093 0.49979 875.592 71.5783 1043.09 86.4999Z" fill="var(--surface)" />
-                </svg>
-
-                <svg v-else width="1772" height="1141" viewBox="0 0 1772 1141" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg  width="1772" height="1141" viewBox="0 0 1772 1141" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1340.36 86.4999C1604.68 104.824 1710.44 185 1772.12 213V1140C1617.27 1130.5 684.375 665 355.418 647.5C26.4609 630 -95.4521 271.878 82.3581 143C278.961 0.500534 449.222 0.50027 627.192 0.5H627.194C765.973 0.49979 1125.13 71.5783 1340.36 86.4999Z" fill="var(--surface)" />
                 </svg>
 
@@ -201,45 +165,29 @@ onUnmounted(() => {
 }
 
 .blob1 {
-    grid-column: 1;
-    grid-row: 1;
-    width: 110%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    margin-left: -10vw;
-    overflow: visible;
-}
-
-.blob2 {
-    grid-column: 1;
-    grid-row: 1;
-    scale: 1.1;
-    width: 110%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    overflow: visible;
+    @apply absolute overflow-hidden left-0;
+    width: 60%;
+    height: 600px;
+    top: 55%;
+    transform: translateY(-50%);
 }
 
 .blob1 svg {
-    width: 100%;
-    height: auto;
-    aspect-ratio: 1274 / 594;
-    max-width: 100%;
-    position: relative;
-    left: 0;
-    top: 0;
-    transform: none;
+    @apply absolute w-full h-full;
+
+}
+
+.blob2 {
+    @apply absolute overflow-hidden right-0;
+    width: 97%;
+    height: 1150px;
+    top: 55%;
+    transform: translateY(-50%);
 }
 
 .blob2 svg {
-    height: auto;
-    max-width: 100%;
-    position: relative;
-    right: 0;
-    top: 0;
-    transform: none;
+    @apply absolute w-full h-full;
+
 }
 
 /* ============================================
@@ -307,18 +255,12 @@ onUnmounted(() => {
 /* ============================================
    RESPONSIVE DESIGN
    ============================================ */
-.blob1 {
-    scale: clamp(0.9, 100vw / 1274, 1.1);
-}
+@media (min-width: 1400px) {
+    .blob2{
+        top: 50%;
+    }
 
-.blob1 svg {
-    max-width: 90vw;
 }
-
-.blob1 svg {
-    max-width: 1274px;
-}
-
 
 @media (min-width: 1600px) {
     .musicnote1{
@@ -334,6 +276,9 @@ onUnmounted(() => {
     }
     .musicnote2{
         scale: 2;
+    }
+    .blob2{
+        top: 35%;
     }
 }
 @media (min-width: 2150px) {
@@ -361,19 +306,25 @@ onUnmounted(() => {
     }
 }
 
-@media (max-width: 1000px) {
-    .blob1 {
-        scale: 1.3;
+@media (max-width: 1023px) {
+    .blob1{
+        width: 120%;
+        height: 600px;
+        top: 35%;
     }
-    .blob2 {
-        scale: 1.2;
+    .blob2{
+        top: 60%;
     }
-}
 
-@media (max-width: 850px) {
-    .blob1 {
-        scale: 2.1;
+}
+@media (max-width: 768px) {
+    .blob2{
+        top: 65%;
     }
+
+}
+@media (max-width: 850px) {
+
     .content1 {
         width: 90%;
         margin-top: 50px;
@@ -381,9 +332,7 @@ onUnmounted(() => {
 }
 
 @media (max-width: 700px) {
-    .blob1 {
-        scale: 2.4;
-    }
+
     .content1 {
         width: 90%;
         margin-top: 100px;
@@ -391,18 +340,14 @@ onUnmounted(() => {
 }
 
 @media (max-width: 500px) {
-    .blob1 {
-        scale: 3.2;
-    }
+
     .content1 {
         margin-top: 100px;
     }
 }
 
 @media (max-width: 400px) {
-    .blob1 {
-        scale: 4.3;
-    }
+
     .content1 {
         width: 90%;
         margin-top: 140px;
